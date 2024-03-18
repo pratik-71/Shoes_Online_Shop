e38dfb360a92aeeba4dfeac9fc6880876363b18a
pratik-71 <pratikdabhade66344@gmail.com>
2024-03-18 23:20:02 +0530

    add review frontend added

---
+++ src/Pages/Products/Products_details/Add_review.jsx
@@ -0,0 +1,108 @@
+import React, { useState } from "react";
+import { Button, Col, Container, Form, Modal, Row } from "react-bootstrap";
+import ProgressBar from "react-bootstrap/ProgressBar";
+
+const Add_review = () => {
+  const [show, setShow] = useState(false);
+  const [rating, setRating] = useState(0);
+
+  const handleStarClick = (star) => setRating(star);
+  const handleClose = () => {
+    setShow(false);
+    setRating(0);
+  };
+  const handleShow = () => setShow(true);
+
+  const renderStars = () => {
+    const stars = [];
+    for (let i = 1; i <= 5; i++) {
+      stars.push(
+        <span
+          key={i}
+          onClick={() => handleStarClick(i)}
+          style={{ cursor: "pointer" }}
+        >
+          {i <= rating ? (
+            <i className="bi bi-star-fill " style={{ color: "gold" }}></i>
+          ) : (
+            <i className="bi bi-star"></i>
+          )}
+        </span>
+      );
+    }
+    return stars;
+  };
+
+  return (
+    <>
+      <Container>
+        <Row>
+          <h6>Product Reviews</h6>
+        </Row>
+        <Row>
+          <label>5 Star</label>
+          <ProgressBar now={70} variant="warning" visuallyHidden />
+        </Row>
+        <Row>
+          <label>4 Star</label>
+          <ProgressBar now={40} variant="warning" visuallyHidden />
+        </Row>
+        <Row>
+          <label>3 Star</label>
+          <ProgressBar now={30} variant="warning" visuallyHidden />
+        </Row>
+        <Row>
+          <label>2 Star</label>
+          <ProgressBar now={25} variant="warning" visuallyHidden />
+        </Row>
+        <Row>
+          <label>1 Star</label>
+          <ProgressBar now={35} variant="warning" visuallyHidden />
+        </Row>
+        <Row>
+          <button
+            onClick={handleShow}
+            className="text-center rounded bg-primary text-white border-0 my-4 py-1"
+          >
+            {" "}
+            Add Review
+          </button>
+        </Row>
+      </Container>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Add Review</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3">
+              <Form.Label>Rating</Form.Label>
+              <div>{renderStars()}</div>
+            </Form.Group>
+            <Form.Group
+              className="mb-3"
+              controlId="exampleForm.ControlTextarea1"
+            >
+              <Form.Control
+                as="textarea"
+                placeholder="Add Review here"
+                rows={3}
+              />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+};
+
+export default Add_review;
--- src/Pages/Products/Products_details/Display_product.jsx
+++ src/Pages/Products/Products_details/Display_product.jsx
@@ -1,151 +1,29 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import "../Products_details/product.css";
-import Display_accordination from "./Display_accordination";
 import Display_other_shoe from "./Display_other_shoe";
-import { Link, useParams } from "react-router-dom";
-import axios from "axios";
+import Display_info from "./Display_info";
+import Add_review from "./Add_review";
+import Display_accordination from "./Display_accordination"
 
 const Display_product = () => {
-
-  const [selectedSize, setSelectedSize] = useState(""); 
-  const [product,setproduct] = useState({})
-  const {id} = useParams()
-  console.log(id)
-
-  const handleSizeChange = (size) => {
-    setSelectedSize(size); 
-  };
-
-  const get_product = async(req,res)=>{
-   try {
-    const response = await axios.get(`http://localhost:3001/products/get_product/${id}`)
-    if(!response){
-      console.log("no data found")
-    }
-    setproduct(response.data)
-   } catch (error) {
-    console.log(error.messege)
-   }
-  }
   
-  useEffect(()=>{
-    get_product()
-  },[])
-
   return (
     <div>
       <Container>
-        <Row>
-          <Col className="my-auto">
-            <img src={product.imageURL} alt="shoe-image" className="img-fluid my-auto" />
+           <Display_info/>
+        <Row className="d-flex justify-content-between">
+          <Col xs={12} md={4} >
+             <Add_review/>
           </Col>
-          <Col className="my-4">
-            <h2 className="text-center">{product.title}</h2>
-            <hr></hr>
 
-            <p>
-              Comfortable, durable and timeless—it's number 1 for a reason. The
-              classic '80s construction is paired with bold details for style
-              that tracks whether you're on the court or on the go.
-              Nature-inspired features like a canvas upper and special logo
-              bloom from its AF-1 roots.
-            </p>
-
-            <h5>MRP : ${product.price}.00 </h5>
-
-
-            <p className="text-secondary">
-              incl. of taxes<br></br>
-              (Also includes all applicable duties)
-            </p>
-
-
-
-
-             <h6>Select Shoe Size</h6>
-            <div className="size-buttons">
-
-              <button
-                className={`size-button ${selectedSize === "6" ? "selected" : ""}`}
-                onClick={() => handleSizeChange("6")}
-              >
-                UK 6
-              </button>
-
-              <button
-                className={`size-button ${selectedSize === "7" ? "selected" : ""}`}
-                onClick={() => handleSizeChange("7")}
-              >
-                UK 7
-              </button>
-
-              <button
-                className={`size-button ${selectedSize === "8" ? "selected" : ""}`}
-                onClick={() => handleSizeChange("8")}
-              >
-                UK 8
-              </button>
-
-              <button
-                className={`size-button ${selectedSize === "9" ? "selected" : ""}`}
-                onClick={() => handleSizeChange("9")}
-              >
-                UK 9
-              </button>
-
-              <button
-                className={`size-button ${selectedSize === "10" ? "selected" : ""}`}
-                onClick={() => handleSizeChange("10")}
-              >
-                UK 10
-              </button>
-
-              <button
-                className={`size-button ${selectedSize === "11" ? "selected" : ""}`}
-                onClick={() => handleSizeChange("11")}
-              >
-                UK 11
-              </button>
-
-              <button
-                className={`size-button ${selectedSize === "12" ? "selected" : ""}`}
-                onClick={() => handleSizeChange("12")}
-              >
-                UK 12
-              </button>
-
-            </div>
-
-            <div className="my-4 d-flex align-items-center justify-content-center text-center">
-              <label className="mx-2">Enter Quantity of product</label>
-              <input
-                  type="number"
-                  rounded
-                  defaultValue={1}
-                  className="form-control search-input b-0 w-25"
-                />
-            </div>
-
-              <div className="text-center column">
-                <Link to="/address">
-                <button className="px-5 py-1 bg-dark rounded text-white">Buy Now</button>
-                </Link>              
-                </div>
-
-              <div className="text-center">
-              <button className="px-4 py-1 border-1 rounded my-3">Add to Cart</button>
-              </div>
-
-
-              <div className="accordintion">
-                <Display_accordination/>
-              </div>
-            
+          <Col  xs={12} md={6}>
+            <Display_accordination/>
           </Col>
         </Row>
+
         <Row>
-          <Display_other_shoe/>
+          <Display_other_shoe />
         </Row>
       </Container>
     </div>
