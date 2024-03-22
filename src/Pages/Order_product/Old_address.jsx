185249446136d7cee9d7e329d8210fb934b2bef1
pratik-71 <pratikdabhade66344@gmail.com>
2024-03-22 21:34:46 +0530

    dlete buton added to old_address

--- src/Pages/Order_product/Old_address.jsx
+++ src/Pages/Order_product/Old_address.jsx
@@ -1,11 +1,12 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Form, FormGroup } from "react-bootstrap";
+import "../Order_product/order.css"
 
 const Old_address = () => {
-  const [old_address, setold_address] = useState([]);
+  const [old_address, setOld_address] = useState([]);
 
-  const get_address = async (req, res) => {
+  const get_address = async () => {
     try {
       const token = localStorage.getItem("Auth-Token");
       const response = await axios.get(
@@ -19,7 +20,7 @@ const Old_address = () => {
 
       if (response) {
         console.log(response);
-        setold_address(response.data);
+        setOld_address(response.data);
       }
     } catch (error) {
       console.log(error);
@@ -29,22 +30,21 @@ const Old_address = () => {
   useEffect(() => {
     get_address();
   }, []);
+
+  const handleDeleteAddress = (id) => {
+    console.log("Deleting address with id:", id);
+  };
+
   return (
     <>
       <div className="old_address">
         <h4 className="text-center">Use Old Address</h4>
-        <FormGroup>
-          <div className="mb-3 my-3 d-flex flex-column">
+        <FormGroup style={{ height: "60vh", overflow: "scroll" }}>
+          <div className="mb-3 my-3 d-flex flex-column p-2">
             {old_address.map((add) => (
               <label
-                key={add.id} // Make sure to assign a unique key to each rendered element
+                key={add.id}
                 className="address-option px-5 py-1 my-1"
-                style={{
-                  border: "1px solid black",
-                  borderRadius: "25px",
-                  display: "flex",
-                  alignItems: "center",
-                }}
               >
                 <input
                   type="radio"
@@ -53,21 +53,28 @@ const Old_address = () => {
                   style={{ marginRight: "10px" }}
                 />
                 <span>
-                 <strong> Name -</strong> {add.fullname},  <strong> PH No. -</strong> {add.phone_number}, 
-                 <strong> State -</strong> {add.state}   <strong> District -</strong> {add.district},  <strong> City/Village -</strong> {add.city_village}, 
-                 <strong> Building/Vilalge -</strong>{add.building_village},  <strong> Pincode -</strong> {add.pincode}
+                  <strong>Name -</strong> {add.fullname}, <strong>PH No. -</strong>{" "}
+                  {add.phone_number}, <strong>State -</strong> {add.state}{" "}
+                  <strong>District -</strong> {add.district}, <strong>City/Village -</strong>{" "}
+                  {add.city_village}, <strong>Building/Vilalge -</strong>
+                  {add.building_village}, <strong>Pincode -</strong> {add.pincode}
                 </span>
-                <span>
-                 
-                </span>
+               
+                <button
+                  className="delete-button"
+                  onClick={() => handleDeleteAddress(add.id)}
+                >
+                 <i class="bi bi-archive-fill"></i>
+                </button>
               </label>
             ))}
-
-            <button className="use-address-button px-5 py-2 rounded bg-primary border-0 text-white">
-              Use This Address
-            </button>
           </div>
         </FormGroup>
+        <div className="text-center my-2">
+          <button className="use-address-button px-5 py-2 rounded bg-primary border-0 text-white">
+            Use This Address
+          </button>
+        </div>
       </div>
     </>
   );
